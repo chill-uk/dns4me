d96d03dc34996c7da91b7f4d6605aced728f506e
Christopher Hill <chill.signup@gmail.com>
2022-07-19 15:54:12 +0200

    Update dns4me.sh

--- dns4me.sh
+++ dns4me.sh
@@ -9,10 +9,10 @@ echo "Updating DNS4ME"
 curl "https://dns4me.net/api/v2/get_hosts/dnsmasq/$dns4meApikey" > /dev/shm/03-dns4me.conf
 
 # Scraping your closest DNS4ME DNS servers
-dnsServer1=$(tail -1 /dev/shm/03-dns4me.conf)
-dnsServer2=$(tail -2 /dev/shm/03-dns4me.conf | head -1)
+dnsServer1=$(tail -2 /dev/shm/03-dns4me.conf | head -1)
+dnsServer2=$(tail -1 /dev/shm/03-dns4me.conf)
 
-# Adding dnsmasq for DNS4ME so your tests pass
+# Adding dnsmasq for dns4me.net so the tests pass
 # http://dns4me.net/check
 cat << EOF >> /dev/shm/03-dns4me.conf
 
@@ -20,14 +20,22 @@ server=/dns4me.net/${dnsServer1##*/}
 server=/dns4me.net/${dnsServer2##*/}
 EOF
 
-diffStatus="$(diff /etc/dnsmasq.d/03-dns4me.conf /dev/shm/03-dns4me.conf)"
-if $diffStatus > /dev/null 2>&1
-then
-    echo "Configs are the same"
-else
-    echo "Config has changed"
-    mv /etc/dnsmasq.d/03-dns4me.conf /tmp/03-dns4me_`date +%d%b%Y`.conf
-    mv /dev/shm/03-dns4me.conf /etc/dnsmasq.d/03-dns4me.conf
-    service dnsmasq restart
-    #curl -s --data "text=DNS4ME changed: $diffStatus" --data "chat_id=$groupId" 'https://api.telegram.org/bot'$botToken'/sendMessage' > /dev/null
+if [ -f /etc/dnsmasq.d/03-dns4me.conf ]; then
+    diffStatus="$(diff /etc/dnsmasq.d/03-dns4me.conf /dev/shm/03-dns4me.conf)"
+    if $diffStatus > /dev/null 2>&1
+    then
+      echo "equal files"
+    else
+      echo "different files"
+      mv /etc/dnsmasq.d/03-dns4me.conf /tmp/03-dns4me_`date +%d%b%Y`.conf
+      mv /dev/shm/03-dns4me.conf /etc/dnsmasq.d/03-dns4me.conf
+      service dnsmasq restart
+      curl -s --data "text=DNS4ME changed: $diffStatus" --data "chat_id=$groupId" 'https://api.telegram.org/bot'$botToken'/sendMessage' > /dev$
+    fi
+else 
+      echo "setting up smartdns"
+      mv /dev/shm/03-dns4me.conf /etc/dnsmasq.d/03-dns4me.conf
+      service dnsmasq restart
+      curl -s --data "text=smartdns setup complete" --data "chat_id=$groupId" 'https://api.telegram.org/bot'$botToken'/sendMessage' > /dev$
 fi
+

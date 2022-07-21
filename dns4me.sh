068b35b4647c98d4050eca96ae3c895bcd26b72d
Christopher Hill <chill.signup@gmail.com>
2022-07-21 11:30:06 +0200

    Update dns4me.sh

--- dns4me.sh
+++ dns4me.sh
@@ -4,38 +4,44 @@
 groupId=xxxxxxxxx
 botToken=xxxxxxxxx:xxxxxx-xxxxxxxxxxxxxxxxxxxx-xxxxxxx
 dns4meApikey=xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+currentConfig=/etc/dnsmasq.d/03-dns4me.conf
+tempConfig=/dev/shm/03-dns4me.conf
+backupConfig=/tmp/03-dns4me_`date +%d%b%Y`.conf
 
 echo "Updating DNS4ME"
-curl "https://dns4me.net/api/v2/get_hosts/dnsmasq/$dns4meApikey" > /dev/shm/03-dns4me.conf
+dnsmasqEntries=$(curl "https://dns4me.net/api/v2/get_hosts/dnsmasq/$dns4meApikey")
 
-# Scraping your closest DNS4ME DNS servers
-dnsServer1=$(tail -2 /dev/shm/03-dns4me.conf | head -1)
-dnsServer2=$(tail -1 /dev/shm/03-dns4me.conf)
+if [ -z "$dnsmasqEntries" ]
+then
+    echo "Could not download dnsmasq entries from dns4me.net. Please check your dns4me Api Key and try again."
+else
 
-# Adding dnsmasq for dns4me.net so the tests pass
-# http://dns4me.net/check
-cat << EOF >> /dev/shm/03-dns4me.conf
+    dnsServer1=$(echo "$dnsmasqEntries" | tail -2 | head -1)
+    dnsServer2=$(echo "$dnsmasqEntries" | tail -1)
 
+cat << EOF > "$tempConfig"
+$dnsmasqEntries
 server=/dns4me.net/${dnsServer1##*/}
 server=/dns4me.net/${dnsServer2##*/}
 EOF
 
-if [ -f /etc/dnsmasq.d/03-dns4me.conf ]; then
-    diffStatus="$(diff /etc/dnsmasq.d/03-dns4me.conf /dev/shm/03-dns4me.conf)"
-    if $diffStatus > /dev/null 2>&1
-    then
-      echo "equal files"
-    else
-      echo "different files"
-      mv /etc/dnsmasq.d/03-dns4me.conf /tmp/03-dns4me_`date +%d%b%Y`.conf
-      mv /dev/shm/03-dns4me.conf /etc/dnsmasq.d/03-dns4me.conf
-      service dnsmasq restart
-      curl -s --data "text=DNS4ME changed: $diffStatus" --data "chat_id=$groupId" 'https://api.telegram.org/bot'$botToken'/sendMessage' > /dev$
+    if [ -f "$currentConfig" ]
+        then
+        diffStatus="$(diff $currentConfig $tempConfig)"
+        if $diffStatus > /dev/null 2>&1
+            then
+                echo "config files are equal"
+            else
+                echo "config files are different"
+                mv $currentConfig $backupConfig
+                mv $tempConfig $currentConfig
+                service dnsmasq restart
+                curl -s --data "text=DNS4ME changed: $diffStatus" --data "chat_id=$groupId" 'https://api.telegram.org/bot'$botToken'/sendMessage' > /dev/n$
+            fi
+        else 
+            echo "No config file found. Setting up smartdns"
+            mv $tempConfig $currentConfig
+            service dnsmasq restart
+            curl -s --data "text=smartdns setup complete" --data "chat_id=$groupId" 'https://api.telegram.org/bot'$botToken'/sendMessage' > /dev$
     fi
-else 
-      echo "setting up smartdns"
-      mv /dev/shm/03-dns4me.conf /etc/dnsmasq.d/03-dns4me.conf
-      service dnsmasq restart
-      curl -s --data "text=smartdns setup complete" --data "chat_id=$groupId" 'https://api.telegram.org/bot'$botToken'/sendMessage' > /dev$
 fi
-
